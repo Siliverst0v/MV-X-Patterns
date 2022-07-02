9248c0c22cd799cfc15f30d1e2477e239161cc55
Siliverst0v <94259002+Siliverst0v@users.noreply.github.com>
2022-07-02 23:41:12 +0300

    Initial Commit

---
+++ MV(X)Patterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MV(X)Patterns
+//
+//  Created by Анатолий Силиверстов on 02.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
